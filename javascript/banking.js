b1a8018e2a9e5a4d2bf36a147c8512686a4edd77
bashirkatugga <93156963+bashirkatugga@users.noreply.github.com>
2021-11-02 16:01:38 -0500

    banking

---
+++ javascript/banking.js
@@ -0,0 +1,20 @@
+function Account(accountName, balance){
+    this.accountName = accountName;
+    this.balance = balance;
+}
+var accountInfo = [];
+
+function populateAccountInfo(){
+    var name = document.getElementById('accountName').value;
+    var balance = document.getElementById('depositAmount').value;
+    accountInfo.push(new Account(name,balance));
+
+    var display = "";
+
+    accountInfo.forEach(eleme=>{
+        display = display.concat(`Account name: `+ `${eleme.accountName}` +` Balance: `+`${eleme.balance}`+`\n`);
+    })
+    document.getElementById("display").innerHTML=display;
+    document.getElementById('accountName').value="";
+    document.getElementById('depositAmount').value="";
+}
